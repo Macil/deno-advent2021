a9ba4eee3c9578b94fea1c212b3d678af90486d4
Chris Cowan <agentme49@gmail.com>
2022-05-26 04:38:04 -0700

    redo aocm_cli

--- aocm_cli.ts
+++ aocm_cli.ts
@@ -1,9 +1,36 @@
+import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/command.ts";
+import { writeAll } from "https://deno.land/std@0.140.0/streams/conversion.ts";
 import { getAocm } from "./aocm.ts";
 
-if (Deno.args.length !== 2 || Deno.args[0] !== "set-cookie") {
-  console.error("Usage: ... set-cookie SESSION");
-  Deno.exit(1);
-}
-
-const aocm = getAocm();
-aocm.setSessionCookie(Deno.args[1]);
+await new Command()
+  .name("aocm")
+  .description("Helper tool for solving Advent of Code with Deno")
+  .version("0.1.0")
+  .command(
+    "set-cookie",
+    "Set the Advent of Code session cookie for later calls",
+  )
+  .arguments("<value:string>")
+  .action(async (_options, value) => {
+    await getAocm().setSessionCookie(value);
+  })
+  .command("clear-data", "Forget the session cookie and cached inputs")
+  .action(() => {
+    throw new Error("Not implemented yet");
+  })
+  .command("get-input", "View the input for a specific day's challenge")
+  .arguments("<year:number> <day:number>")
+  .action(async (_options, year, day) => {
+    const input = await getAocm().getInput(year, day);
+    await writeAll(Deno.stdout, new TextEncoder().encode(input));
+  })
+  .command(
+    "safe-run",
+    "Run a solution script in a safely-sandboxed environment",
+  )
+  .option("--deno-flags=<flags:string>", "Pass extra flags to Deno")
+  .arguments("<script_arg:string>")
+  .action((_options, _scriptArg) => {
+    throw new Error("Not implemented yet");
+  })
+  .parse(Deno.args);
